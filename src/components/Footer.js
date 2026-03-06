84c9b9928bc8bd7f6424afc76615f4ce7eb22f60
HAMZA SELMAN <hamzeksselman2@gmail.com>
2026-03-06 23:51:00 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+function Footer() {
+  return (
+    <footer style={{ marginTop: '50px', padding: '30px', backgroundColor: '#f4f4f4', textAlign: 'center' }}>
+      <p>&copy; 2026 Ethio-Marketplace. All rights reserved.</p>
+      <div style={{ marginTop: '10px' }}>
+        <Link to="/about" style={{ margin: '0 10px', color: '#666' }}>About Us</Link>
+        <Link to="/terms" style={{ margin: '0 10px', color: '#666' }}>Terms of Use</Link>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
