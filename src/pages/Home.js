41f42769dc133326631eb84beca31d1edcbb502b
HAMZA SELMAN <hamzeksselman2@gmail.com>
2026-03-06 23:51:28 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to Ethio-Market</h1>
+      <p>Find the best products from local sellers.</p>
+    </div>
+  );
+}
+export default Home;
