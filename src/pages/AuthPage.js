db59bf5c8a3d74b5b9192aa16089ba69846dde13
HAMZA SELMAN <hamzeksselman2@gmail.com>
2026-03-06 23:52:22 +0300

    Update AuthPage.js

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function AuthPage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Login / Sign Up</h1>
+      <p>Welcome back! Please enter your details.</p>
+    </div>
+  );
+}
+export default AuthPage;
