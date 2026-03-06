0c227cdb907de325b2004d89aadaaed15a411c96
HAMZA SELMAN <hamzeksselman2@gmail.com>
2026-03-06 23:51:45 +0300

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function AboutUs() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>About Us</h1>
+      <p>Ethio-Market is a bridge between local sellers and buyers.</p>
+    </div>
+  );
+}
+export default AboutUs;
