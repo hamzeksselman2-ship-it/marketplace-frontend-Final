923592d7a822ce400d9df3d53da3a322388cd451
HAMZA SELMAN <hamzeksselman2@gmail.com>
2026-03-06 23:52:05 +0300

    Update Terms.js

--- src/pages/Terms.js
+++ src/pages/Terms.js
@@ -1,1 +1,11 @@
+import React from 'react';
 
+function Terms() {
+  return (
+    <div style={{ padding: '40px' }}>
+      <h1>Terms of Use</h1>
+      <p>Please use our platform responsibly and honestly.</p>
+    </div>
+  );
+}
+export default Terms;
